fbf6e439dcc25a386ec70bc3c6417167764567ee
pslaski <slaskip@gmail.com>
2014-06-16 14:21:26 +0200

    fixed bug with generating play_evolutions table

--- src/main/scala/generators/utils/ModelProvider.scala
+++ src/main/scala/generators/utils/ModelProvider.scala
@@ -1,16 +1,22 @@
 package generators.utils
 
+import scala.slick.jdbc.meta.createModel
+
 class ModelProvider(config : Config) {
 
   private val jdbcDriver = config.jdbcDriver
   private val url = config.url
   private val user = config.user
   private val password = config.password
+  private val excluded = Seq("play_evolutions")
 
   lazy val slickDriver = DriverLoader.slickDriver(jdbcDriver)
   lazy val slickDriverPath = DriverLoader.slickDriverPath(jdbcDriver)
 
   lazy val db = slickDriver.simple.Database.forURL(url,driver=jdbcDriver, user = user, password = password)
-  lazy val model = db.withSession(slickDriver.createModel(_))
+  lazy val model = db.withSession { implicit session =>
+    val tables = slickDriver.getTables.list.filterNot(t => excluded.exists(_.equalsIgnoreCase(t.name.name)))
+    createModel( tables, slickDriver )
+  }
 
 }
